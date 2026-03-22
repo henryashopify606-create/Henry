dbd6128f43cf6399b75fb17324f123edcb31df88
henryashopify606-create <henrya.shopify606@gmail.com>
2026-03-22 01:22:35 +0000

    Update bot owner and owner number in settings

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'cursed', // Your name
+  ownerNumber: '233539665528', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
